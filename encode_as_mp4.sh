1aa37bdf6109ab095d2ade65c3233a314c001419
Joshua Stephenson <josh@brightmediums.com>
2021-09-26 11:30:22 -0400

    Now skips encoding if already in mp4 and cleans up original files (semi-dangerous)

--- encode_as_mp4.sh
+++ encode_as_mp4.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+# Cleans up original if already encoded
+cleanup(){
+    directory="$(dirname "$1")/"
+    echo "DIRECTORY: $directory"
+    echo "SOURCE: $source_dir"
+    if [ "$source_dir" == "$directory" ]
+    then
+        echo "Removing file only: $1"
+        $remove_command "$1"
+    else
+        echo "Removing original file and directory: $directory"
+        $remove_command -r "$directory"
+    fi
+}
+
 # Takes a filename (mp4 file)
 #  imports it into TV.app using the `open` command
 #  Assumes TV.app is the default file type for MP4 files
@@ -14,11 +29,68 @@ import(){
   osascript -e 'tell application "TV"
 pause
 end tell' > /dev/null 2>&1
+  cleanup "$1"
+  echo "successfully imported $(basename "$1") :)"
   sleep 1
+}
+
+# Moves MP4 file passed into this script to target dir
+move_file(){
+  if [ -z "$1" ]
+  then
+    echo "You must provide a filename to encode_file()"
+    exit 1
+  fi
+  echo "Encoding file: $1"
+
+  # Add trailing slash to directory if necessary
+  trailing_slash=$(echo "$directory" | egrep "/$")
+  if [ -z "$trailing_slash" ]
+  then
+    directory="$directory/"
+  fi
+
+  # strip the directory, leaving just the filename
+  short_file="$(basename "$1")"
+
+  # strip extension and replace with mp4
+  short_file="${short_file%.*}.mp4"
+
+  newfilename="$directory$short_file"
+
+  echo "Moving $newfilename"
+  if [ -f "$newfilename" ]
+  then
+    echo "$newfilename already exists."
+    remove_flag=$(echo "$2" | grep "-x")
+    if [ -n "$remove_flag" ]
+    then
+        echo "TESTING: Removing original file: $1"
+        # $remove_command "$1"
+    fi
+  else
+    echo "about to move"
+
+    # looking for subtitles
+    subtitles="$(find "$(dirname "$1")" -name "*.srt" )"
+    if [ -n "$subtitles" ]
+    then
+      echo "Subtitles Found: $subtitles"
+      $move_command "$subtitles" "$directory"
+    fi
+    $move_command "$1" "$newfilename"
+    if [ -f "$newfilename" ]
+    then
+      import "$newfilename"
+    else
+      echo "failed moving $newfilename :("
+    fi
+  fi
 }
 
 # Encodes Non MP4 file passed into this script into  mp4
 encode_file(){
+  echo ""
   if [ -z "$1" ]
   then
     echo "You must provide a filename to encode_file()"
@@ -38,28 +110,26 @@ encode_file(){
 
   # strip extension and replace with mp4
   short_file="${short_file%.*}.mp4"
-  echo "short_file: $short_file"
 
   newfilename="$directory$short_file"
 
-  echo "Encoding $newfilename"
   if [ -f "$newfilename" ]
   then
-    echo "$newfilename already exists. not overwriting."
+    echo "$newfilename already exists. Removing original."
+    cleanup "$1"
   else
-    echo "about to encode"
     subtitles="$(find "$(dirname "$1")" -name "*.srt" )"
     if [ -n "$subtitles"]
     then
       echo "SUBTITLES Found: $subtitles"
       subtitles="-s $subtitles"
     fi
-    $command -i "$1" -o "$newfilename" $arguments "$subtitles"
+    echo "Encoding $newfilename"
+    $handbrake -i "$1" -o "$newfilename" $arguments "$subtitles"
     if [ -f "$newfilename" ]
     then
       import "$newfilename"
-      echo "successfully encoded $newfilename :)"
-      echo "Waiting 5 minutes before continuing to be nice to hardware."
+      echo "Waiting 5 minutes before continuing."
       sleep 300
     else
       echo "failed encoding $newfilename :("
@@ -69,17 +139,32 @@ encode_file(){
 
 echo "Preparing to Encode"
 
-command=$(which HandBrakeCLI)
-if [ ! -f "$command" ]
+handbrake=$(which HandBrakeCLI)
+if [ ! -f "$handbrake" ]
 then
   echo "No HandBrakeCLI command found"
   exit 1
 fi
 arguments="-e x264 -q 20 -B 160"
 
+move_command=$(which mv)
+if [ ! -f "$move_command" ]
+then
+  echo "No mv command found"
+  exit 1
+fi
+
+remove_command=$(which rm)
+if [ ! -f "$remove_command" ]
+then
+  echo "No rm command found"
+  exit 1
+fi
+
 # The file to re-encode
 filename=$1
 directory=$2
+source_dir=$3
 
 if [ -z "$1" ]
 then
@@ -99,6 +184,12 @@ if [ -n "$is_sample" ]
 then
   echo "Skipping sample file: $filename"
 else
-  encode_file "$filename"
+  is_mp4=$(echo "$filename" | grep -i '*mp4')
+  if [ -n "$is_mp4" ]
+  then
+    move_file "$filename"
+  else
+    encode_file "$filename"
+  fi
 fi
 
