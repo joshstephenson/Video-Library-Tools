8cc435d8732bf5f7c5bbed325f86db57003aa72c
Joshua Stephenson <josh@brightmediums.com>
2021-09-27 09:21:55 -0400

    Adding exit code to usage() and updated comments

--- video_library_encoder.sh
+++ video_library_encoder.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-dir=$(dirname ${BASH_SOURCE[0]})
 encode="$dir/encode_as_mp4.sh"
 move="$dir/move_mp4.sh"
 
@@ -9,10 +8,9 @@ target_dir="$2"
 
 usage() {
     echo "$0 [Source folder] [Destination folder]"
-    exit
+    exit 1
 }
 
-# Find non-MP4 files in the directory specified on the command line
 if [ -z "$source_dir" ]
 then
   usage
@@ -25,7 +23,7 @@ fi
 
 echo "Looking for video files in $1"
 
-# Find non-mp4 video files in the directory passed via command line.
+# Find video files in the directory passed via command line.
 # For each file, encode it
 
 find $source_dir \( -name "*mp4" -o -name "*avi" -o -name "*mkv" -o -name "*m4v" -o -name "*mpeg" -o -name "*divx" \) -exec \
