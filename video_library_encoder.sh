5bc0a3e0b6629e6292e2be9030c24aeafc86c58b
Joshua Stephenson <josh@brightmediums.com>
2021-10-21 14:48:07 -0400

    bug with checking for handbrake running

--- video_library_encoder.sh
+++ video_library_encoder.sh
@@ -3,6 +3,7 @@
 dir="$(dirname ${BASH_SOURCE[0]})"
 encode="$dir/encode_as_mp4.sh"
 move="$dir/move_mp4.sh"
+handbrake=$(which HandBrakeCLI)
 
 source_dir="$1"
 target_dir="$2"
@@ -31,7 +32,7 @@ fi
 echo "Looking for video files in $1"
 
 # First check to see if HandBrake is running already
-if [ $(ps ax | grep HandBrakeCLI | egrep -v grep | wc -l) -gt 0 ]
+if [ $(ps ax | grep $handbrake | egrep -v grep | wc -l) -gt 0 ]
 then
     echo "HandBrakeCLI is already running. Aborting."
     exit 0
